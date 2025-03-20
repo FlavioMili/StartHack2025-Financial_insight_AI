2666689493453b771643e1caf4739fe59e274010
Francesco Caracciolo <67018178+FrancescoCaracciolo@users.noreply.github.com>
2025-03-20 16:58:46 +0100

    Add endpoints

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, send_file, request
 from .pipeline import ResponsePipeline
 import os
+from .speech.order_speakers import classify_speaker
 
 app = Flask(__name__)
 ASSETS_FOLDER = "assets"
@@ -8,6 +9,8 @@ ASSETS_FOLDER = "assets"
 VOCAL_CHAT_MEM = {0: [{"role": "user", "content": "Why my tesla shares dropped?"}]}
 
 TEXT_CHAT_MEM = {}
+TEMPORARY_CHAT_MEM = {}
+PIPELINES = {}
 
 @app.route('/getClientData/<int:id>', methods=['GET'])
 def getClientData(id):
@@ -15,10 +18,6 @@ def getClientData(id):
     if not os.path.exists(file_path):
         return jsonify({"error": "File not found"}), 404
     return send_file(file_path, mimetype='application/json')
-
-@app.route("/add_transcript/<int:id>", methods=["POST"])
-def add_transcript(id):
-    pass
 
 @app.route('/query/<int:id>', methods=['GET'])
 def query(id):
@@ -28,20 +27,50 @@ def query(id):
         return jsonify({"error": "No JSON data provided"}), 400
     return jsonify({"received": response}), 200
 
+@app.route('/get_user/<int:id>', methods=['GET'])
+def get_user(id):
+    user_info = open(f"../assets/client_{str(id)}.json", "r").read()
+    return user_info, 200
+
 @app.route('/chat/<int:id>', methods=['POST'])
 def chat(id):
     prompt = request.form.get("prompt")
     if not prompt:
         return jsonify({"error": "Prompt is required"}), 400
     try:
-        history = TEXT_CHAT_MEM.get(id, [])
-        history.append({"role": "user", "content": prompt})
-        TEXT_CHAT_MEM[id] = history
-        model = ResponsePipeline(id)
+        history = TEXT_CHAT_MEM.get(id, []).append({"role": "user", "content": prompt})
+        pipeline = PIPELINES.get(id, None)
+        if pipeline is None:
+            model = ResponsePipeline(id)
+        else:
+            model = pipeline
         response = model.get_answer(history)
+
     except Exception as e:
         return jsonify({"error": str(e)}), 500
     return jsonify({"prompt": prompt, "response": response}), 200
+
+@app.route('/add_interaction/<int:id>', methods=['POST'])
+def add_interaction(id):
+    sentence = request.form.get("sentence")
+    if not sentence:
+        return jsonify({"error": "Message is required"}), 400
+    try:
+        text = TEMPORARY_CHAT_MEM.get(id, "")
+        text += sentence
+        if PIPELINES.get(id, None) is None:
+            model = ResponsePipeline(id)
+        else:
+            model = PIPELINES.get(id)
+        response = classify_speaker(model.llm, text, VOCAL_CHAT_MEM[id])
+        if len(response) > len(VOCAL_CHAT_MEM[id]):
+            VOCAL_CHAT_MEM[id] = response
+        else:
+            VOCAL_CHAT_MEM[id] += response 
+        
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    return jsonify({}), 200
 
 if __name__ == '__main__':
     os.makedirs(ASSETS_FOLDER, exist_ok=True)  
